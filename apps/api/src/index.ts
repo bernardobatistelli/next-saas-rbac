5df0abbc5d528132ae04610e0c38bd2e6d21a02f
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-05-16 15:02:34 -0300

    feat : defining permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,3 @@
 import { defineAbilityFor } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
-
-const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOtherUsers)
-console.log(userCannotDeleteOtherUsers)
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
