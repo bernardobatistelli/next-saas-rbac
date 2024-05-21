591d6ce2e737d4fa19ff74420d8e30d3d5e256ae
Bernardo Batistelli <bebatistelli254@gmail.com>
2024-05-21 10:46:37 -0300

    feat : authenticating with github

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -10,6 +10,7 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
@@ -48,6 +49,7 @@ app.register(authenticateWithPassword)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
+app.register(authenticateWithGithub)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running!')
